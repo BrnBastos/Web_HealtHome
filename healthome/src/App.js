3f32fdc5dbb010c2e2cf787eadcaaf0658a374aa
BrnBastos <rm550416@fiap.com.br>
2023-11-22 16:41:43 -0300

    criação do banco de dados

--- healthome/src/App.js
+++ healthome/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
-import Header from './componentes/header/header';
+import Header from './componentes/header/header.js';
+import HeaderW from './componentes/header/headerWnav.js';
+import Login from './componentes/cadastro/login.js';
+import Cadastrar from "./componentes/cadastro/cadastrar.js";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Header/>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<div><HeaderW/><Login/></div>} />
+          <Route path='/Home' element={<div><Header/></div>} />
+          <Route path='/Login' element={<div><HeaderW/><Login/></div>} />
+          <Route path='/Cadastro' element={<div><HeaderW/><Cadastrar/></div>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
