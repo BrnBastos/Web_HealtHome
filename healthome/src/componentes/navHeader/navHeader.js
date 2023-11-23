0d60552cf50b93ff5d084cff962fcd29bc9f8381
BrnBastos <rm550416@fiap.com.br>
2023-11-23 13:37:58 -0300

    estilização nav

--- healthome/src/componentes/navHeader/navHeader.js
+++ healthome/src/componentes/navHeader/navHeader.js
@@ -1,10 +1,25 @@
+import { Link, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledLink = styled(Link)`
+    text-decoration:none;
+    color:inherit;
+`
+
 function NavHeader(){
+    const navigate = useNavigate();
+
+    const Out = () => {
+        navigate('/login')
+    }
+
     return(
         <nav>
             <ul className='navegacaoHeader'>
-                <li className='liNav'>Home</li>
-                <li className='liNav'>Quem Somos</li>
-                <li className='liNav'>Contato</li>
+                <StyledLink to="/cadastro"><li className='liNav'>Home</li></StyledLink>
+                <StyledLink to="/cadastro"><li className='liNav'>Quem Somos</li></StyledLink>
+                <StyledLink to="/cadastro"><li className='liNav'>Contato</li></StyledLink>
+                <li className='liNavOut'><button onClick={Out} className="btnOut">Sair</button></li>
             </ul>
         </nav>
     );
